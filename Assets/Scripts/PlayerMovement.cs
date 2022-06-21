c8c1f3484be1e97938990b873cf1890336b793ed
Aersiterliu <1940826732@qq.com>
2022-06-21 12:23:54 +0800

    basic shoot complete

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletHell
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public GameObject[] guns;
+        public float speed;
+        private Vector2 input;
+        private Vector2 mousePos;
+        private Animator animator;
+        new private Rigidbody2D rigidbody;
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            rigidbody.velocity = input.normalized * speed;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x > transform.position.x)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            }
+
+            if (input != Vector2.zero)
+                animator.SetBool("isMoving", true);
+            else
+                animator.SetBool("isMoving", false);
+        }
+
+
+    }
+}
